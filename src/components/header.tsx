4d10297aae2da790844af91d4ec8fedd3e0f8559
goldmaster631026 <goldmaster631026@gmail.com>
2025-04-27 00:08:55 -0400

    add phone and chatbot

--- src/components/header.tsx
+++ src/components/header.tsx
@@ -3,7 +3,7 @@ export default function Header() {
     return (
         <>
         <div className="h-16 bg-black text-white flex items-center justify-center font-bold text-500">
-            Welcome to Pack3000 !
+            Welcome to Pack3000 24/7 Call & Chat Support Center !
         </div>
 
         <div className='h-auto w-auto max-w-full rounded-lg shadow-lg mx-auto my-4 flex justify-center items-center'>
